942499d3ee6827ab71b955026dd70cb74807ff29
Sourav Sinha <sousin18@in.ibm.com>
2020-11-02 16:26:12 +0530

    counter with constructor

---
+++ Assignment-3/assignment3.js
@@ -0,0 +1,16 @@
+function Counter(count) {
+  this.count = count;
+  this.updateValue = function (val) {
+    this.count = val + this.count;
+  };
+  this.up = function () {
+    this.updateValue(1);
+    console.log(this.count);
+  };
+  this.down = function () {
+    this.updateValue(1);
+    console.log(this.count);
+  };
+}
+
+let spinner = new Counter(5);
