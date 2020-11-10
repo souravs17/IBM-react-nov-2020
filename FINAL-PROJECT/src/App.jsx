fc125fa87bf69bd80d201875315321e5452e8724
Sourav Sinha <sousin18@in.ibm.com>
2020-11-10 11:51:41 +0530

    final project

---
+++ FINAL-PROJECT/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import store from "./store";
+import Products from "./products";
+import Categories from "./categories";
+import Home from "./home";
+import Navigation from "./navigation";
+import Cart from "./cart";
+const App = () => {
+  return (
+    <Router>
+      <Provider store={store}>
+        <Navigation />
+        <Switch>
+          <Route path="/categories" component={Categories} />
+          <Route path="/products" component={Products} />
+          <Route path="/cart" component={Cart} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Provider>
+    </Router>
+  );
+};
+
+export default App;
---
+++ FINAL-PROJECT/src/cart/views/CartTotal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CartTotal = ({ total }) => {
+  return <div className="total_text">Total : Rs {total}</div>;
+};
+
+export default CartTotal;
